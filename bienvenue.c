612f9cd57a3d6312be27534897ec57b5a48338be
PacCol <50033325+PacCol@users.noreply.github.com>
2019-09-21 21:53:03 +0200

    Update bienvenue.c

--- bienvenue.c
+++ bienvenue.c
@@ -7,6 +7,7 @@ void bienvenue()
 {
   printf("\nBienvenue dans le pendu !\n");
   printf("crée par Pacôme Colinet\n\n");
-  printf("Les accents ne seront pas pris en charge\n");
-  printf("Chaque étoile représente une lettre que vous n'avez pas encore trouvé.\n\n");
+  printf("Les accents ne seront pas pris en charge...\n");
+  printf("Chaque étoile représente une lettre que vous n'avez pas encore trouvé.");
+  printf("Exemple : *ty*o pour stylo si vous avez déjà trouvé le t, le y, et le o\n\n");
 }
